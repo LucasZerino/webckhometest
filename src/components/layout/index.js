85dd8c793e80d8349ae2c06ba09305b1c51f4364
Lucas Zerino <101360532+LucasZerino@users.noreply.github.com>
2022-04-05 12:42:50 -0300

    primeiro commit

---
+++ src/components/layout/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import PageRevealer from '../page-revealer'
+import Navbar from '../navbar'
+import Footer from '../footer'
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../../scss/abstract.scss'
+
+class Layout extends React.Component {
+
+  componentDidUpdate() {
+    window.location.reload(false);
+  }
+
+
+  render() {
+
+    const { children } = this.props
+    
+    
+
+    return (
+      <div id="main">
+        <Navbar scroll={this.props.isHome ? true : false} sections={this.props.sections} />
+        <div>
+          {children}
+        </div>
+        <Footer />
+      </div>
+    )
+  }
+}
+
+export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,54 @@
-import React from "react"
+import React from 'react'
+import Layout from '../components/layout/index'
+import { graphql } from 'gatsby'
+import Helmet from 'react-helmet'
+import Hero from '../sections/hero/Hero'
+import AboutOne from '../sections/about/AboutOne'
+import ServicesOne from '../sections/services/ServicesOne.js'
+import TestimonialsThree from '../sections/testimonials/TestimonialsThree.js'
+import ContactCreative from '../sections/contact/ContactCreative.js'
+import ClientsOne from '../sections/clients/ClientsOne'
 
-export default function Home() {
-  return <div>Hello world!</div>
+
+
+
+class Index extends React.Component {
+
+  render() {
+
+    const { site } = this.props.data
+
+    return (
+      <div id='home'>
+        <Helmet>
+        <title>{site.meta.title}</title>
+        <meta name="description" content={site.meta.description} />
+        </Helmet>
+        <Layout
+          isHome={true}
+          sections={['home', 'sobre nos', 'serviços', 'avaliações', 'contato']}
+        >
+        <Hero />
+        <AboutOne/>
+        <ServicesOne />
+        <TestimonialsThree />
+        <ClientsOne />  
+        <ContactCreative />
+        </Layout>
+      </div>
+    )
+  }
 }
+
+export default Index
+
+export const creativeOneQuery = graphql`
+  query {
+    site {
+      meta: siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`
